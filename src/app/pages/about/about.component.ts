8f48081c53567b9976a7edf06da96405fb9ca777
Lisbeth Camacaro <lisbethcamacaro90@gmail.com>
2023-06-26 15:03:10 -0600

    Se agrega servicio con firebase

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+
+  constructor(public infoService: InfoPaginaService){
+
+  }
+
+
+  ngOnInit() {
+ 
+  }
 
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,15 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[]= [];
 
   constructor(private httpClient: HttpClient) {
 
-    //Leer el archivo Json
-    this.httpClient.get('assets/data/data-page.json')
-      .subscribe( (resp: InfoPagina )=> {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp.email);
-      });
+    this.cargarInfo();
+    this.cargarEquipo();
   }
-}
+
+      private cargarInfo(){
+        //Leer el archivo Json
+        this.httpClient.get('assets/data/data-page.json')
+        .subscribe( (resp: InfoPagina )=> {
+          this.cargada = true;
+          this.info = resp;
+        });
+      }
+
+      
+      private cargarEquipo(){
+
+        this.httpClient.get('https://angular-html-ce0ae-default-rtdb.firebaseio.com/equipo.json')
+        .subscribe( (resp : any)=> {
+          this.equipo= resp;
+        });
+      
+      }
+  }
+
